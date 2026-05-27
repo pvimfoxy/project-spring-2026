7c769ffa0313df82ee738093befd1ad80b14f6d5
pvimfoxy <vpovoroznyuk@nes.ru>
2026-05-27 11:36:02 +0300

    Update base_parser.py

--- target audience/parsers/base_parser.py
+++ target audience/parsers/base_parser.py
@@ -13,5 +13,14 @@ def fetch(self, *args, **kwargs):
 
 @abstractmethod
 def parse(self, raw_data):
-    """Превращает сырые данные в структуру"""
+    """Превращает данные в структуру"""
     pass
+
+def to_dataframe(self) -> pd.DataFrame:
+    return pd.DataFrame(self.data)
+
+def save(self, df: pd.DataFrame = None):
+     if df is None:
+        df = self.to_dataframe()
+     df.to_csv(self.output_path, index=False, encoding='utf-8-sig')
+     print(f"Сохранено {len(df)} записей в {self.output_path}")
