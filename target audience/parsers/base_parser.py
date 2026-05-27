ddca05c6ed6f4ef1bfb685c434ceb1d9534dc413
pvimfoxy <vpovoroznyuk@nes.ru>
2026-05-27 11:34:48 +0300

    Update base_parser.py

--- target audience/parsers/base_parser.py
+++ target audience/parsers/base_parser.py
@@ -1,2 +1,17 @@
 from abc import ABC, abstractmethod
 import pandas as pd
+
+class BaseParser(ABC):
+    def __init__(self, output_path: str):
+        self.output_path = output_path
+        self.data = []
+
+@abstractmethod
+def fetch(self, *args, **kwargs):
+    """Забирает данные из источника"""
+    pass
+
+@abstractmethod
+def parse(self, raw_data):
+    """Превращает сырые данные в структуру"""
+    pass
